eb87f6cff0006b15728a6003bb25cf0f01b51361
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-04-23 09:51:08 +0000

    [Add] 3-13 フラッシュメッセージの実装 Destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,13 +24,14 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-      flash[:notice] = "Book was successfully updated."
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book.id)
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
 
